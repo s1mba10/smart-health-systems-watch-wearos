3ae5749c27fce526dd50c7bd5ad3089339e63fe3
s1mba <danilmpower@gmail.com>
2025-09-29 12:40:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/smart_health_systems_sw/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.smart_health_systems_sw.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun SmarthealthsystemsswTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
